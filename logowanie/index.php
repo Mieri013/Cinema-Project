05b814afc26329b6c04b4dc85d5259d1d45ef032
Anna Więzik <92309502+AlaAniaPatryk@users.noreply.github.com>
2021-11-24 15:51:21 +0100

    logowanie
    Ukończona strona logowania. Posiada już finalny układ strony, po wpisaniu poprawnych danych logowania przesyła na stronę główną.

---
+++ logowanie/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="shortcut icon" href="icon.png">
+	<link rel="stylesheet" href="style.css">
+	<title>Zaloguj się</title>
+</head>
+<body>
+
+<form action="zaloguj.php" method="post">
+	<input type="text" placeholder="Login" name="login">
+	<input type="password" placeholder="Hasło" name="haslo"><br />
+	<input type="submit" value="Zaloguj się">
+</form>
+<div>
+	<a href="index.html">Zarejestruj się</a>
+	<a href="index.html">Zapomniałeś hasła?</a>
+</div>
+
+</body>
+</html>
---
+++ logowanie/zaloguj.php
@@ -0,0 +1,40 @@
+<?php 
+
+session_start();
+
+require_once "conncet.php";
+
+$polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+
+if($polaczenie->connect_errno!=0)
+{
+	echo "Error ".$polaczenie->connect_errno;
+}else{
+	$login = $_POST['login'];
+	$haslo = $_POST['haslo'];
+
+	$mysql="SELECT * FROM uzytkownicy WHERE user='$login' AND password='$haslo'";
+
+	if ($rezultat= @$polaczenie->query($mysql)) 
+	{
+
+		$ilu_userow = mysqli_num_rows($rezultat);
+		if($ilu_userow==1)
+		{
+			$wiersz = $rezultat->fetch_assoc();
+			$user = $wiersz['user'];
+
+			$rezultat->free_result();
+			header('Location: glowna/index.html');
+		}else{
+
+		}
+
+	}
+	$polaczenie->close();
+}
+
+
+
+
+?>
