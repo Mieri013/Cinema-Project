edae62d14774c8ee764f6820b640e2e1add386d8
Anna Więzik <92309502+AlaAniaPatryk@users.noreply.github.com>
2022-01-06 14:10:48 +0100

    Add files via upload

---
+++ program2/Bilet.cs
@@ -0,0 +1,16 @@
+﻿namespace tests
+{
+    public class Bilet
+    {
+        public Product(string rodzaj, int rzad, int miejsce)
+        {
+            Rodzaj = rodzaj;
+            Rzad = rzad;
+			Miejsce = miejsce;
+        }
+
+        public string Rodzaj { get; }
+        public int Rzad { get; }
+		public int Miejsce { get; }
+    }
+}
---
+++ program2/TicketsList.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests
+{
+    public class TicketsList
+    {
+        private List<Bilet> _bilet = new List<Bilet>();  
+
+        public object Size 
+            => _bilet.Count;
+
+        public void AddTicket(Bilet bilet)
+            => _bilet.Add(product);
+
+        public void RemoveTicket(int ticketIndex)
+		{
+			if(ticketIndex >=0 && ticketIndex < _bilet.Size)
+				_bilet.RemoveAt(ticketIndex);
+		}
+    }
+}
