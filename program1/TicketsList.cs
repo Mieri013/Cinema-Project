1a400c989be4ced91f907384a61974c2786cb095
Anna Więzik <92309502+AlaAniaPatryk@users.noreply.github.com>
2022-01-06 14:09:14 +0100

    Add files via upload

---
+++ program1/TicketsList.cs
@@ -0,0 +1,16 @@
+﻿namespace Tests
+{
+    public class TicketsList
+    {
+        private List<Bilet> _bilet = new List<Bilet>();  
+
+        public object Size 
+            => _bilet.Count;
+
+        public void AddTicket(Bilet bilet)
+            => _bilet.Add(product);
+
+        public void RemoveTicket(int ticketIndex)
+			=> _bilet.RemoveAt(ticketIndex);
+    }
+}
